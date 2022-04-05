ef8264a4f02bdbb396bab438719736850d2b073a
Avery King <kavery086@gmail.com>
2022-04-05 04:21:11 -0400

    api queried

--- Program.cs
+++ Program.cs
@@ -1,16 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using ToDoApi.Models;
 
+var myAllowSpecificOrigins = "myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-builder.Services.AddDbContext<ToDoContext>(opt =>
+builder.Services.AddCors(options =>
 {
-    opt.UseInMemoryDatabase("ToDoList");
+    options.AddPolicy(name: myAllowSpecificOrigins, builder => builder.WithOrigins("*", "http://localhost:3000"));
 });
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ToDoContext>(opt => { opt.UseInMemoryDatabase("ToDoList"); });
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment()) app.UseDeveloperExceptionPage();
+app.UseCors(myAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
--- React/src/App.js
+++ React/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
 
+const API_URL = 'https://localhost:7135/api/todo/';
 function App() {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL)
+        .then(res => res.json())
+        .then(data => {
+          setList(data);
+        })
+  },[setList]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div style={{textAlign:'center',marginTop:'25vh'}}>
+    <h2>To-Do List</h2>
+     <h3>This is a simple to-do list built with ASP.NET Core.</h3>
+
+       {list.map(item => (
+           <p key={item.id}>{item.name}</p>
+       ))}
+
+   </div>
   );
 }
 
